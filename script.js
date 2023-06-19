b627f88851f56469efc5a486624c884ffdc5cf8c
Sleep <137116881+HiSleep@users.noreply.github.com>
2023-06-19 23:56:56 +0200

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-
-
 function checkPassword() {
-    const password = document.getElementById('password').value;
-    if (password === 'sleep') {
-        document.getElementById('login').style.display = 'none';
-        document.getElementById('main-content').style.display = 'block';
+    var password = document.getElementById("password").value;
+    if(password == "sleep") {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("main-content").style.display = "block";
+        startClock();
     } else {
-        alert('Contraseña incorrecta');
+        alert("Contraseña incorrecta. Inténtalo de nuevo.");
     }
+}
+
+function startClock() {
+    setInterval(() => {
+        let hours = Math.floor(Math.random() * 24);
+        let minutes = Math.floor(Math.random() * 60);
+        let seconds = Math.floor(Math.random() * 60);
+        document.getElementById("clock").innerText = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+    }, 1000);
+}
+
+function pad(number) {
+    return number.toString().padStart(2, '0');
 }
